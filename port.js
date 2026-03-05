425f05f1e3191f4a8ad7415a7458d8bf83835542
lash-abi <abiabilash815@gmail.com>
2026-03-05 10:29:50 +0530

    My portfolio website

---
+++ port.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".multiple-text",{
+    strings: [
+        "AI Engineering student...!",
+        "Web Designer...!",
+        "Editor...!",
+        "Youtuber...!",
+        "Content Creator...!",
+        "Blogger...!"
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop : true
+});
